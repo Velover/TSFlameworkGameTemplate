a3434f521b77c3adf5bfa895501bc6898efa0289
Catargo <cobauper@gmail.com>
2025-03-08 13:33:59 +0100

    first commit

---
+++ src/client/runtime.client.ts
@@ -0,0 +1,7 @@
+import { Flamework } from "@flamework/core";
+
+Flamework.addPaths("src/client/Components");
+Flamework.addPaths("src/client/Controllers");
+Flamework.addPaths("src/shared/Components");
+
+Flamework.ignite();
---
+++ src/server/runtime.server.ts
@@ -0,0 +1,7 @@
+import { Flamework } from "@flamework/core";
+
+Flamework.addPaths("src/server/Components");
+Flamework.addPaths("src/server/Services");
+Flamework.addPaths("src/shared/Components");
+
+Flamework.ignite();
